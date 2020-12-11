5320a0619a766d32abe67c6f81e51a37bc8a426f
rafaelsurfce <rafaelsurfce@hotmail.com>
2020-12-11 17:16:14 -0300

    11/12/2020

---
+++ atividades/atividade03/logins.sh
@@ -0,0 +1,14 @@
+#1
+grep -v 'sshd' auth.log.1
+
+#2
+grep -E 'sshd[[[:digit:]]*]:[[:space:]]*Accepted' auth.log.1
+
+#3
+
+grep -E 'sshd[[[:digit:]]*]:[[:space:]]*Disconnected from authenticating user root' auth.log.1 
+
+#4
+
+grep -E 'Dec[[:space:]]*4[[:space:]]*(18.*|19:00.*).*sshd[[[:digit:]]*]:[[:space:]]*Accepted' auth.log.1
+
