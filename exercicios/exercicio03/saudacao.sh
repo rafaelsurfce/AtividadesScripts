7357c93f8f77a701cc40d3280ac6f2737b445284
rafaelsurfce <rafaelsurfce@hotmail.com>
2020-12-17 13:57:41 -0300

    Erros corridigos

--- exercicios/exercicio03/saudacao.sh
+++ exercicios/exercicio03/saudacao.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Correção: Parcialmente OK.
-echo "Olá" $(hostname), " 
+echo "Olá" $(whoami), " 
 Hoje é dia" $(date +%d)", dos mês" $(date +%m)" do ano de" $(date +%G) | tee -a saudacao.log
 
 
