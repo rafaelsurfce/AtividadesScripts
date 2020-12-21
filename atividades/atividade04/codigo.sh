55c836d78855e6722bea1950dbdb516b73ba7e24
rafaelsurfce <rafaelsurfce@hotmail.com>
2020-12-21 17:30:38 -0300

    21/12/2020

--- atividades/atividade04/codigo.sh
+++ atividades/atividade04/codigo.sh
@@ -5,9 +5,9 @@ sed -i 's/\/bin\/python/\/usr\/bin\/python3/g' atividade04.py
 #2
 sed -i 's/nota/NOTA/g' atividade04.py | sed -i 's/Final/FINAL/g' atividade04.py
 #3
-sed -i 's\import  os\import time\' atividade04.py 
+sed -i '3a \import time' atividade04.py 
 #4
-sed -i '9a \   print(time.ctime())' atividade04.py 
+sed -i '10a \   print(time.ctime())' atividade04.py 
  
 
 
