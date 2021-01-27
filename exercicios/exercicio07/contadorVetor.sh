76ca8d4fb9d02b6661aff9964915cf19c48aa31c
Rafaelsurfce <Rafaelsurfce@alu.ufc.br>
2021-01-26 21:05:20 -0300

    questão 2

---
+++ exercicios/exercicio07/contadorVetor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+numeros=()
+indice=0
+while [ true ]
+do
+	read -p  "Digite um numero para armazenar no vetor, ou q para listar o tamanho do vetor: " comando
+	if [[ $comando != "q" ]]
+	then
+		numeros[$indice]=$comando
+		indice=$((indice+1))
+		echo "numero adicionado com sucesso"
+	elif [[ $comando == "q" ]]
+	then
+		echo "tamanho é ${#numeros[@]}"
+		break
+	else
+	
+		echo "comando nao reconhecido"
+	fi
+	
+done
