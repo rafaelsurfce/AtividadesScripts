88a4c4c9bac989514a468cffe20dc1e6ff347e38
Rafaelsurfce <Rafaelsurfce@alu.ufc.br>
2021-02-10 21:53:29 -0300

    Só pra mostrar que eu tentei

---
+++ atividades/atividade11/hosts.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cont=1
+ip=null
+nome=null
+touch hosts.db
+getopts "a:i:r:d:l" OPTVAR
+
+case $OPTVAR in
+a)
+	cont=0
+	nome=$OPTARG
+	;;
+i)
+	cont=0
+	ip=$OPTARG
+	;;
+d)
+	cont=0
+	echo 'ta excluindo' 
+	sed '/'$OPTARG'.*/d' hosts.db
+	;;
+l)
+	cont=0
+	echo 'ta listando'
+	cat hosts.db
+	;;
+r)
+	grep .*$OPTARG hosts.db | cut -f1 -d' '
+	;;
+esac
+if [[ $nome != null  ]] && [[ $ip != null ]]
+then
+	echo '$nome $ip' >> hosts.db
+	nome=null
+	ip=null
+fi
