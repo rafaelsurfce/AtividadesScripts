647b344f17bdc3d592a02a61ce8422d553c00928
Rafaelsurfce <Rafaelsurfce@alu.ufc.br>
2021-03-02 00:49:49 -0300

    servidorWeb.sh

--- atividades/atividade14/servidorWeb.sh
+++ atividades/atividade14/servidorWeb.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 imagem=ami-03d315ad33b9d49c4 
-grupoSecurity=$(aws ec2 describe-security-groups --query "SecurityGroups[0].GroupId" --output text)
+grupoSecurity=$(aws ec2 create-security-group --group-name "atividade14" --description "para a atividade14" --output text)
 subnetId=$(aws ec2 describe-subnets --query "Subnets[0].SubnetId" --output text)
 chave=$1
+
+aws ec2 authorize-security-group-ingress --group-id $grupoSecurity --protocol tcp --port 80 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $grupoSecurity --protocol tcp --port 22 --cidr 0.0.0.0/0
+#aws ec2 authorize-security-group-ingress --group-id $grupoSecurity --protocol icmp --cidr 0.0.0.0/0
+
+
 
 echo "Criando servidor..."
 idInstancia=$(aws ec2 run-instances --image-id $imagem --instance-type "t2.micro" --security-group-ids $grupoSecurity --subnet $subnetId --key-name $chave --query "Instances[0].InstanceId" --output text)
